226746226f0079c5353ee42014aeb12b90211a37
natsu0001 <talukdarabhishek687@gmail.com>
2025-11-14 10:27:48 +0530

    corsl in starlay

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,7 @@ const App = () => {
   return (
     <Layout title='SweetDreams Cafe'>
       <Hero />
-      <Star
-        title="The Ultimate Venilla Fudge Cake"
-        description="Indulge in our rich and decadent Venilla Fudge Cake, crafted with layers of moist chocolate sponge, velvety fudge frosting, and a hint of espresso to enhance the chocolate flavor. Perfect for any celebration or a special treat!"
-        image="/assets/bgcake.png"
-        imageAlt="Chocolate Fudge Cake"
-      />
+      <Star />
     </Layout>
   );
 };
